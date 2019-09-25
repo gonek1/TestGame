e4688f3767644d730707516895d45815e380dfd8
gonek1 <moskinoEU@yandex.ru>
2019-09-26 00:37:20 +0300

    update mini

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -7,6 +7,8 @@ public class Dealer : MonoBehaviour
     [SerializeField] List<Item> items = new List<Item>();
     [SerializeField] Transform content;
     [SerializeField] GameObject CellPrefab;
+    [SerializeField] GameObject ShopPanel;
+    TypeOfAction typeOfAction = TypeOfAction.Use;
     void Start()
     {
         Render();
@@ -22,6 +24,41 @@ public class Dealer : MonoBehaviour
         {
            var cell =  Instantiate(CellPrefab, content);
             cell.GetComponent<DealerCell>().AddItem(item);
+        }
+    }
+    public void OpenShop()
+    {
+        ShopPanel.SetActive(true);
+        Controller.instance.SetMove(false);
+        Controller.instance.SetOpenInv(false);
+    }
+    public void CloseShop()
+    {
+        ShopPanel.SetActive(false);
+        Controller.instance.SetMove(true);
+        Controller.instance.SetOpenInv(true);
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player")&& Input.GetKeyDown(KeyCode.E))
+        {
+            OpenShop();
+            InfoManager.instance.CloseInfoPanel();
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            CloseShop();
+            InfoManager.instance.CloseInfoPanel();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            InfoManager.instance.ShowInfoPanel(typeOfAction, this.gameObject);
         }
     }
 }
